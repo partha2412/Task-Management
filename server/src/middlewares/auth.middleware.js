e7586b8a3e628dcafc67ba7fc9f992187771f7f8
Partha Singh <parthasingh35@gmail.com>
2026-09-06 12:47:34 +0530

    Refactor isAuth middleware to simplify token retrieval by removing commented-out cookie handling code

--- server/src/middlewares/auth.middleware.js
+++ server/src/middlewares/auth.middleware.js
@@ -3,20 +3,20 @@ import User from "../models/User.js";
 
 export const isAuth = async (req, res, next) => {
     try {
-        const cookies = req.headers.cookie;
+        // const cookies = req.headers.cookie;
 
-        if (!cookies) {
-            return res.status(401).json({
-                success: false,
-                message: "Unauthorized. No token provided.",
-            });
-        }
+        // if (!cookies) {
+        //     return res.status(401).json({
+        //         success: false,
+        //         message: "Unauthorized. No token provided.",
+        //     });
+        // }
 
-        const token = cookies
-            .split("; ")
-            .find((cookie) => cookie.startsWith("token="))
-            ?.split("=")[1];
-
+        // const token = cookies
+        //     .split("; ")
+        //     .find((cookie) => cookie.startsWith("token="))
+        //     ?.split("=")[1];
+        const token = req.cookies?.token;
         if (!token) {
             return res.status(401).json({
                 success: false,
